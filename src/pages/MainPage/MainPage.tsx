9ee4e173bca2478af5236cc2895e3a9144b35f8b
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-09-20 22:30:07 +0900

    메인페이지 UI 수정

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,232 +1,191 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { CgGym } from "react-icons/cg";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { BsStarFill } from "react-icons/bs";
 import styles from "./MainPage.module.css";
-import { collection, addDoc, onSnapshot, orderBy, query, Timestamp, deleteDoc, doc } from "firebase/firestore";
-import DatePicker from "react-datepicker";
-import Texts from "../../components/Texts/Texts";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
-import db from "../../api/firebase";
+import db, { onUserStateChange } from "../../api/firebase";
 import dateString from "../../utils/Date";
-import { BsFillTrashFill } from "react-icons/bs";
 import { useSelector } from "react-redux/es/hooks/useSelector";
-import Flower from "../../components/Flower/Flower";
-import { RootState } from "../../utils/Store";
+import { RootState, changeUser } from "../../utils/Store";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { collection, deleteDoc, doc, onSnapshot, orderBy, query } from "firebase/firestore";
+import Timer from "../../components/Timer/Timer";
+import { pickedDataProps } from "../../utils/type";
 
-interface TextArrProps {
-  id: string;
-  text: string;
-  createdAt: string;
-  creatorId: string;
-  postId: string;
-  timeStamp: string;
-}
-[];
+// interface TextArrProps {
+//   id: string;
+//   text: string;
+//   createdAt: string;
+//   creatorId: string;
+//   postId: string;
+//   timeStamp: string;
+// }
+// [];
 
 export default function MainPage(): React.ReactElement {
-  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const user = useSelector((state: RootState) => state.user);
+  const [pickedDatas, setPickedDatas] = useState<pickedDataProps[]>([]);
+  const navigate = useNavigate();
+  const [useStopWatch, setUseStopWatch] = useState(false);
+  // const [text, setText] = useState("");
+  // const [texts, setTexts] = useState<TextArrProps[]>([]);
+  // const textarea = useRef<HTMLTextAreaElement>(null);
+  // const [oldRecord, setOldRecord] = useState<TextArrProps[]>([]);
+  // const textId = useSelector((state: RootState) => state.textId.textId);
+  // const [addModal, setAddModal] = useState(false);
 
-  const [text, setText] = useState("");
-  const [texts, setTexts] = useState<TextArrProps[]>([]);
-  const textarea = useRef<HTMLTextAreaElement>(null);
-  const [oldRecord, setOldRecord] = useState<TextArrProps[]>([]);
-  const textId = useSelector((state: RootState) => state.textId.textId);
-  const [addModal, setAddModal] = useState(false);
-  const timeStamp = Timestamp.now();
-  //Date Picker
-  // 하루 전날 계산하는 공식 ( 기본값이 하루 전날로 세팅)
-  const currentDate = new Date();
-  const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
-  const oneDayBefore = new Date(currentDate.getTime() - oneDayInMilliseconds);
-
-  const [startDate, setStartDate] = useState<Date | null>(oneDayBefore);
-
-  //startDate 값 형식 바꾸기
-  const year = startDate?.getFullYear();
-  const month = String((startDate as Date).getMonth() + 1).padStart(2, "0");
-  const day = String(startDate?.getDate()).padStart(2, "0");
-
-  const pickerDate = `${year}-${month}-${day}`;
-
-  //서버에서 데이터 받아와서 texts 에 배열로 저장
   useEffect(() => {
+    if (changeUser) {
+      onUserStateChange((user: { uid: string }) => {
+        dispatch(changeUser(user?.uid));
+      });
+    }
+  }, [changeUser]);
+
+  const handleStopWatch = () => {
+    setUseStopWatch((prev) => !prev);
+  };
+  // //서버에서 데이터 받아와서 texts 에 배열로 저장
+  // useEffect(() => {
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: "smooth",
+  //   });
+  //   const q = query(collection(db, "details"), orderBy("timeStamp", "desc"));
+  //   onSnapshot(q, (snapshot) => {
+  //     const textArr = snapshot.docs.map((doc) => ({
+  //       id: doc.id,
+  //       ...doc.data(),
+  //     })) as TextArrProps[];
+  //     setTexts(textArr.filter((t: TextArrProps) => t.creatorId === user?.uid && t.createdAt === dateString));
+  //     setOldRecord(
+  //       textArr.filter((t: TextArrProps) => {
+  //         return t.creatorId === user?.uid && t.createdAt === pickerDate;
+  //       })
+  //     );
+  //   });
+  // }, [setTexts, user?.uid, setOldRecord, pickerDate]);
+
+  // // 메인페이지 기록 버튼 (서버에 데이터 저장)
+  // const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   if (text.length > 0) {
+  //     await addDoc(collection(db, "details"), {
+  //       text,
+  //       createdAt: dateString,
+  //       creatorId: user?.uid,
+  //       timeStamp,
+  //     });
+  //   }
+  //   setText("");
+  // };
+
+  const handleRecord = () => {
+    navigate("/record");
+  };
+  useEffect(() => {
+    if (!user) return;
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
-    const q = query(collection(db, "details"), orderBy("timeStamp", "desc"));
+    const q = query(collection(db, "RecordFinish"), orderBy("timeStamp", "desc"));
     onSnapshot(q, (snapshot) => {
-      const textArr = snapshot.docs.map((doc) => ({
+      const pickedArr = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
-      })) as TextArrProps[];
-      setTexts(textArr.filter((t: TextArrProps) => t.creatorId === user?.uid && t.createdAt === dateString));
-      setOldRecord(
-        textArr.filter((t: TextArrProps) => {
-          return t.creatorId === user?.uid && t.createdAt === pickerDate;
-        })
-      );
+      })) as pickedDataProps[];
+      setPickedDatas(pickedArr.filter((picked) => picked.creatorId === user?.uid && picked.createdAt === dateString));
     });
-  }, [setTexts, user?.uid, setOldRecord, pickerDate]);
+  }, []);
 
-  // 메인페이지 기록 버튼 (서버에 데이터 저장)
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (text.length > 0) {
-      await addDoc(collection(db, "details"), {
-        text,
-        createdAt: dateString,
-        creatorId: user?.uid,
-        timeStamp,
-      });
-    }
-    setText("");
-  };
-
-  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setText(e.target.value);
-    if (textarea.current) {
-      textarea.current.style.height = "auto";
-      textarea.current.style.height = textarea.current.scrollHeight + "px";
-    }
-  };
-
-  const deleteText = () => {
+  const handleDeleteData = (data: pickedDataProps) => {
     const ok = window.confirm("정말 삭제하실꺼죠?");
 
     if (ok) {
       //서버에서 데이터 삭제하기
-      deleteDoc(doc(db, "details", textId));
-      setTexts(texts.filter((text) => text.id !== textId));
+      deleteDoc(doc(db, "RecordFinish", data.id));
+      setPickedDatas(pickedDatas.filter((picked) => picked.id !== data.id));
     }
   };
-
-  const dragOver = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
-  };
-  const moveToPage = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target instanceof HTMLImageElement) {
-      if (e.target.alt === "운동내역화면") navigate("/my");
-      else navigate("/share");
-    }
-  };
-
   return (
     <>
-      <Flower />
       <div className={styles.wrapper}>
         <Header />
-
-        <section className={styles.backgroundImage}>
-          <div>
-            <p>기록을 바탕으로 성장해보아요!</p>
-            <p>당신은 이미 성장중입니다</p>
-
-            <div onClick={moveToPage}>
-              <img src="./운동내역화면.png" className={styles.mainImage_one} alt="운동내역화면" />
-              <img src="./게시물작성화면.png" className={styles.mainImage_two} alt="게시물작성화면" />
-            </div>
-          </div>
-        </section>
-
-        <article>
-          <div className={styles.title}>
-            <h1>오늘의 Health</h1>
-            <form onSubmit={onSubmit} className={styles.form}>
-              <textarea
-                ref={textarea}
-                className={styles.width}
-                rows={1}
-                cols={30}
-                name="text"
-                placeholder="나의 운동을 기록해보아요 💪 : (예시) 벤치프레스 30kg  4sets  10reps "
-                maxLength={1200}
-                value={text}
-                onChange={onChange}
-                wrap="on"
-              />
-              {user && (
-                <button className={styles.input} disabled={false}>
-                  기록
-                </button>
-              )}
-
-              {!user && (
-                <button className={styles.input} disabled={false}>
-                  로그인 필수 👌
-                </button>
-              )}
-            </form>
-            <div
-              className={styles.trash}
-              onDrop={deleteText}
-              onDragOver={dragOver}
-              onMouseOver={() => setAddModal(true)}
-              onMouseLeave={() => setAddModal(false)}
-            >
-              <BsFillTrashFill />
-            </div>
-            {
-              <span className={styles.trashExplain} style={{ opacity: addModal ? "1" : "0" }}>
-                운동기록을 드래그 해서 삭제하실 수 있어요!
-              </span>
-            }
-          </div>
-
-          <section className={styles.textArticle}>
-            {oldRecord.length > 0 ? (
-              <div className={styles.textList}>
-                {user && (
-                  <section className={styles.datePickerSection}>
-                    <div className={styles.textTitle}>지난 기록</div>
-                    <div className={styles.datePicker}>
-                      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} maxDate={oneDayBefore} />
-                    </div>
-                  </section>
-                )}
-                {oldRecord.map((record) => {
-                  return (
-                    record.creatorId === user?.uid &&
-                    record.createdAt === pickerDate && (
-                      <div className={styles.textArea}>
-                        <Texts textObj={record} key={record.id} isOwner={record.creatorId === user?.uid} />
-                      </div>
-                    )
-                  );
-                })}
+        <article className={styles.mainContainer}>
+          <div className={styles.mainBody}>
+            <section className={styles.userImgAndId}>
+              <div className={styles.userImg}>
+                <FaUserCircle />
               </div>
-            ) : (
-              <>
-                <div className={styles.textList}>
-                  {user && (
-                    <section className={styles.datePickerSection}>
-                      <div className={styles.textTitle}>지난 기록</div>
-                      <div className={styles.datePicker}>
-                        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} maxDate={oneDayBefore} />
+              <div>
+                <h3 className={styles.userId}>{user.uid}님</h3>
+                <span className={styles.setExercise}>운동을 설정해주세요</span>
+              </div>
+            </section>
+
+            <section className={styles.mainFunctions}>
+              <div className={styles.recordFunc} onClick={handleRecord}>
+                <CgGym className={styles.gymImg} />
+                <p>운동기록</p>
+              </div>
+              <div className={styles.chatFunc}>
+                <BsFillChatDotsFill className={styles.chatImg} />
+                <p>채팅하기</p>
+              </div>
+              <div className={styles.popularFunc} onClick={handleStopWatch}>
+                <BsStarFill className={styles.popImg} />
+                <p>스톱워치</p>
+              </div>
+            </section>
+          </div>
+          {useStopWatch && (
+            <div className={styles.timer}>
+              <Timer />
+            </div>
+          )}
+          <section className={styles.mainContent}>
+            <p className={styles.contentTitle}>오늘의 운동기록</p>
+            <section className={styles.dataWrapper}>
+              {pickedDatas.map((data, i) => {
+                return (
+                  <>
+                    <section className={styles.dataContainer} key={i}>
+                      <div>
+                        {i + 1}. {data.name}
                       </div>
+                      <section className={styles.dataContent}>
+                        <div>총 {data.kg.length}세트</div>
+                        <div style={{ display: "flex", flexDirection: "column" }}>
+                          {data.kg.map((d) => (
+                            <div> {d + "kg"} </div>
+                          ))}
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "column" }}>
+                          {data.reps.map((d) => (
+                            <div> {d + "회"} </div>
+                          ))}
+                        </div>
+                        <div className={styles.deleteContent} onClick={() => handleDeleteData(data)}>
+                          X
+                        </div>
+                      </section>
                     </section>
-                  )}
-                  <div className={styles.textArea}>
-                    <Texts textObj={{ text: "휴식 하신 날입니다! 😎", id: " " }} isOwner={true} />
-                  </div>
-                </div>
-              </>
-            )}
+                  </>
+                );
+              })}
+            </section>
+          </section>
 
-            {texts.length > 0 && (
-              <div className={styles.textList}>
-                <div className={styles.textTitle}>오늘의 기록</div>
-                {texts.map((text) => {
-                  return text.creatorId === user?.uid && <Texts textObj={text} key={text.id} isOwner={text.creatorId === user?.uid} />;
-                })}
-              </div>
-            )}
+          <section className={styles.mainFootArea}>
+            <Footer />
           </section>
         </article>
-        <div className={styles.footerArea}>
-          <Footer />
-        </div>
       </div>
     </>
   );
