e15250355e3d0fb41116ef42e9737cc783c9715d
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-09-20 22:37:11 +0900

    타입지정, Redux 전역변수 지정

--- src/utils/Store.tsx
+++ src/utils/Store.tsx
@@ -1,4 +1,5 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { RecordingProps } from "./type";
 
 const textId = createSlice({
   name: "textId",
@@ -20,12 +21,48 @@ const user = createSlice({
   },
 });
 
+const pickedDatas = createSlice({
+  name: "pickedDatas",
+  initialState: { pickedDatas: [] as RecordingProps[] },
+  reducers: {
+    addPickedDatas: (state, action) => {
+      const isDuplicate = state.pickedDatas.some((data) => data.name === action.payload.name);
+      if (!isDuplicate) {
+        state.pickedDatas.push(action.payload);
+      }
+    },
+    removePickedDatas: (state, action) => {
+      state.pickedDatas = state.pickedDatas.filter((data) => data.name != action.payload.name);
+    },
+    resetPickedDatas: (state) => {
+      state.pickedDatas = [];
+    },
+  },
+});
+const rowCountData = createSlice({
+  //이거 로컬스토리지에 저장해서 해보기
+  name: "rowCountData",
+  initialState: { count: 0 },
+  reducers: {
+    addCount: (state) => {
+      state.count = state.count + 1;
+    },
+    minusCount: (state) => {
+      state.count = state.count - 1;
+    },
+  },
+});
+
 export const { changeData } = textId.actions;
 export const { changeUser } = user.actions;
+export const { addPickedDatas, removePickedDatas, resetPickedDatas } = pickedDatas.actions;
+export const { addCount, minusCount } = rowCountData.actions;
 const store = configureStore({
   reducer: {
     textId: textId.reducer,
     user: user.reducer,
+    pickedDatas: pickedDatas.reducer,
+    rowCountData: rowCountData.reducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
---
+++ src/utils/type.ts
@@ -0,0 +1,25 @@
+export interface pickedDataProps {
+  id: string;
+  name: string;
+  reps: string[];
+  kg: string[];
+  createdAt: string;
+  creatorId: string;
+}
+[];
+
+export interface RecordingProps {
+  name: string;
+  sub: string;
+  type: string;
+  url: string;
+}
+[];
+
+export interface TypeDatas {
+  type: string;
+  sub: string;
+  name: string;
+  url: string;
+}
+[];
