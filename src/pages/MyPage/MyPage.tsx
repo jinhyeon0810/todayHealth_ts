83c53c1954942dfb57e80adb925694bcfcbf499e
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-09-20 22:35:49 +0900

    캘린더 페이지 추가

--- src/pages/MyPage/MyPage.tsx
+++ src/pages/MyPage/MyPage.tsx
@@ -7,26 +7,18 @@ import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import db from "../../api/firebase";
-import TextComponent from "../../components/MyPage/TextComponent";
-import Flower from "../../components/Flower/Flower";
 import { useSelector } from "react-redux";
 import { RootState } from "../../utils/Store";
-
-interface textArrProps {
-  id: string;
-  text: string;
-  createdAt: string;
-  creatorId: string;
-}
-[];
+import { useNavigate } from "react-router-dom";
+import { pickedDataProps } from "../../utils/type";
 
 export default function MyPage(): React.ReactElement {
   const user = useSelector((state: RootState) => state.user);
+  const navigate = useNavigate();
 
+  //캘린더 선택한 날짜 정보표시
   const [startDate, setStartDate] = useState<Date | null>(new Date());
-  const [texts, setTexts] = useState<textArrProps[]>([]);
-  console.log(texts);
-
+  const [pickedDatas, setPickedDatas] = useState<pickedDataProps[]>([]);
   const year = startDate?.getFullYear();
   const month = String((startDate as Date)?.getMonth() + 1).padStart(2, "0");
   const day = String(startDate?.getDate()).padStart(2, "0");
@@ -35,27 +27,26 @@ export default function MyPage(): React.ReactElement {
 
   //서버에서 운동기록 데이터 가져오기
   useEffect(() => {
+    if (!user) return;
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
-    const q = query(collection(db, "details"), orderBy("timeStamp", "desc"));
+    const q = query(collection(db, "RecordFinish"), orderBy("timeStamp", "desc"));
     onSnapshot(q, (snapshot) => {
-      const textArr = snapshot.docs.map((doc) => ({
+      const pickedArr = snapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
-      })) as textArrProps[];
-      console.log(textArr);
-      setTexts(
-        textArr.filter((t: textArrProps) => {
-          return t.creatorId === user?.uid && pickerDate === t.createdAt;
-        })
-      );
+      })) as pickedDataProps[];
+      setPickedDatas(pickedArr.filter((picked) => picked.creatorId === user?.uid && picked.createdAt === pickerDate));
     });
-  }, [setTexts, user?.uid, startDate, pickerDate]);
+  }, [pickerDate]);
+
+  const handleRecordPage = () => {
+    navigate("/record");
+  };
   return (
     <>
-      <Flower />
       <div className={styles.wrapper}>
         <Header />
         <article className={styles.layout}>
@@ -67,26 +58,41 @@ export default function MyPage(): React.ReactElement {
               진행한 운동 <GiMuscleUp /> / <span className={styles.exerciseDay}>{pickerDate}</span>
             </h1>
             <ul className={styles.desc}>
-              {texts?.length > 0 ? (
-                texts?.map((text) => {
+              {pickedDatas?.length > 0 ? (
+                pickedDatas?.map((data, i) => {
                   return (
-                    text.creatorId === user?.uid &&
-                    pickerDate === text.createdAt && (
-                      <div className={styles.recordComponent}>
-                        <TextComponent text={text} />
-                      </div>
+                    data.creatorId === user?.uid &&
+                    pickerDate === data.createdAt && (
+                      <section className={styles.dataContainer}>
+                        <div>
+                          {i + 1}. {data.name}
+                        </div>
+                        <section className={styles.dataContent}>
+                          <div>총 {data.kg.length}세트</div>
+                          <div style={{ display: "flex", flexDirection: "column" }}>
+                            {data.kg.map((d) => (
+                              <div> {d + "kg"} </div>
+                            ))}
+                          </div>
+                          <div style={{ display: "flex", flexDirection: "column" }}>
+                            {data.reps.map((d) => (
+                              <div> {d + "회"} </div>
+                            ))}
+                          </div>
+                        </section>
+                      </section>
                     )
                   );
                 })
               ) : (
-                <div className={styles.noDesc}> 완료한 운동이 없습니다.😓 </div>
+                <button className={styles.noDesc} onClick={handleRecordPage}>
+                  운동기록을 추가해주세요 😁{" "}
+                </button>
               )}
             </ul>
           </div>
-        </article>
-        <div className={styles.footerArea}>
           <Footer />
-        </div>
+        </article>
       </div>
     </>
   );
