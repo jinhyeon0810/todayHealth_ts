51b71c5f07d73c7d0200ca3aa00bdae2a436acae
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-09-21 23:20:23 +0900

    설정 페이지 추가작성

---
+++ src/pages/Sort/SortContainer.tsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+import styles from "./Sort.module.css";
+import SortPresenter from "./SortPresenter";
+import { logout, onUserStateChange } from "../../api/firebase";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, changeUser } from "../../utils/Store";
+import CannotAccess from "../../components/CannotAccess/CannotAccess";
+import { getAuth, onAuthStateChanged, updateProfile } from "firebase/auth";
+
+export default function SortContainer(): React.ReactElement {
+  const user = useSelector((state: RootState) => state);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+
+  const [logoutModalOpen, setLogoutModalOpen] = useState(false);
+  const [nickNameModalOpen, setNickNameModalOpen] = useState(false);
+  const [newNick, setNewNick] = useState("");
+
+  const handleLogOut = () => {
+    setLogoutModalOpen(true);
+  };
+
+  console.log(user);
+
+  useEffect(() => {
+    if (changeUser) {
+      onUserStateChange((user: { uid: string; displayName: string; photoURL: string }) => {
+        dispatch(changeUser({ uid: user.uid, displayName: user.displayName, photoURL: user.photoURL }));
+      });
+    }
+  }, [changeUser]);
+
+  console.log(loading);
+  const handleModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!user) return;
+    else if ((e.target as HTMLElement).innerText === "취소") {
+      setLogoutModalOpen(false);
+      setNickNameModalOpen(false);
+    } else if ((e.target as HTMLElement).innerText === "로그아웃") {
+      logout().then(() => {
+        onUserStateChange(() => {
+          dispatch(
+            changeUser({
+              uid: null,
+              displayName: null,
+              photoURL: null,
+            })
+          );
+          navigate("/");
+        });
+      });
+    } else if ((e.target as HTMLElement).innerText === "확인") {
+      if (newNick.trim().length === 0) return;
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user !== null) {
+          updateProfile(user, {
+            displayName: newNick,
+          })
+            .then(() => {
+              setLoading(true);
+              window.location.reload();
+            })
+            .catch((error) => console.error("프로필 업데이트 오류", error));
+        }
+      });
+    }
+    setNickNameModalOpen(false);
+    setLoading(false);
+  };
+
+  const handleNickName = () => {
+    setNickNameModalOpen(true);
+  };
+
+  const onChangeNickName = (e: { target: { value: React.SetStateAction<string> } }) => {
+    setNewNick(e.target.value);
+  };
+
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      {user.user.uid && !loading && (
+        <>
+          <SortPresenter handleLogOut={handleLogOut} user={user} handleNickName={handleNickName} />
+          {logoutModalOpen && (
+            <section className={styles.modal}>
+              <div>로그아웃할까요?</div>
+              <div className={styles.modalButtons}>
+                <button className={styles.okModal} onClick={handleModal}>
+                  로그아웃
+                </button>
+                <button className={styles.cancelModal} onClick={handleModal}>
+                  취소
+                </button>
+              </div>
+            </section>
+          )}
+          {nickNameModalOpen && (
+            <section className={styles.modal}>
+              <div>새로운 닉네임을 입력해주세요</div>
+              <input type="text" value={newNick} onChange={onChangeNickName} placeholder="닉네임" className={styles.inputStyle} />
+              <div className={styles.modalButtons}>
+                <button className={styles.okModal} onClick={handleModal}>
+                  확인
+                </button>
+                <button className={styles.cancelModal} onClick={handleModal}>
+                  취소
+                </button>
+              </div>
+            </section>
+          )}
+        </>
+      )}
+      {!user.user.uid && loading && <CannotAccess />}
+    </div>
+  );
+}
---
+++ src/pages/Sort/SortPresenter.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "./Sort.module.css";
+import Footer from "../../components/Footer/Footer";
+import { FaUserCircle } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FiLogOut } from "react-icons/fi";
+
+interface Props {
+  handleLogOut: () => void;
+  user: {
+    user: { uid: string | null; displayName: string | null; photoURL: string | null };
+  };
+  handleNickName: () => void;
+}
+
+export default function SortPresenter(props: Props): React.ReactElement {
+  const { handleLogOut, user, handleNickName } = props;
+  return (
+    <article className={styles.wrapper}>
+      <section>
+        <header className={styles.header}>설정</header>
+        <div className={styles.body}>
+          <section className={styles.userProfile}>
+            <div>나</div>
+            <div className={styles.buttons} onClick={handleNickName}>
+              <button className={styles.content}>
+                <span className={styles.icons}>
+                  <FaUserCircle />
+                </span>
+                {user.user.displayName}
+              </button>
+              <button className={styles.arrow}>{`>`}</button>
+            </div>
+          </section>
+
+          <section className={styles.etc}>
+            <div>기타</div>
+            <div className={styles.buttons}>
+              <button className={styles.content}>
+                <span className={styles.icons}>
+                  <MdEmail />
+                </span>
+                건의사항 메일 보내기
+              </button>
+              <button className={styles.arrow}>{`>`}</button>
+            </div>
+            <div className={styles.buttons} onClick={handleLogOut}>
+              <button className={styles.content}>
+                <span className={styles.icons}>
+                  <FiLogOut />
+                </span>
+                로그아웃
+              </button>
+              <button className={styles.arrow}>{`>`}</button>
+            </div>
+          </section>
+        </div>
+      </section>
+      <Footer />
+    </article>
+  );
+}
