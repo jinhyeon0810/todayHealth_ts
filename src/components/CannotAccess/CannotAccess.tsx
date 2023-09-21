c71badbe32e191abf4e5f9fb0314fdf71feae91d
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-09-21 23:18:38 +0900

    로그인 후 이용 UI 생성

---
+++ src/components/CannotAccess/CannotAccess.tsx
@@ -0,0 +1,14 @@
+import styles from "./CannotAccess.module.css";
+import { useNavigate } from "react-router-dom";
+
+export default function CannotAccess() {
+  const navigate = useNavigate();
+  return (
+    <section className={styles.cannotAccess}>
+      <div>로그인 하셔야 이용할 수 있습니다</div>
+      <div onClick={() => navigate("/")} className={styles.backToPage}>
+        되돌아가기
+      </div>
+    </section>
+  );
+}
