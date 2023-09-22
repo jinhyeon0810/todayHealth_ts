024f16ff8a60f4f5cb2e71fed3910482ef53b31e
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-09-22 21:10:04 +0900

    게시물 card컴포넌트 ui 등 수정

--- src/components/TextCard/TextCard.tsx
+++ src/components/TextCard/TextCard.tsx
@@ -11,17 +11,12 @@ interface Props {
 
 export default function TextCard({ product }: Props): React.ReactElement {
   return (
-    <div className={styles.style}>
+    <section className={styles.style}>
       <div className={styles.component}>
         <div className={styles.imageArea}>
           <img className={styles.images} src={product.url} />
         </div>
-        <div className={styles.contentArea}>
-          <div className={styles.type}> Category : {product.category}</div>
-          <div className={styles.title}>{product.title.length > 6 ? `${product.title.substr(0, 6) + "..."}` : product.title} </div>
-        </div>
-        <div className={styles.userId}> id : {product.creatorId.substr(0, 4)}</div>
       </div>
-    </div>
+    </section>
   );
 }
