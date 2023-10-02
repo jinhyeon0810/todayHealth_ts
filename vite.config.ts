5bf41663a71bb63e60a81469b35926fd4c8033f8
Jinhyeon Kim <jinhyeon0810@gmail.com>
2023-10-02 17:43:38 +0900

    채팅Room UI수정 및 vite config 수정 등

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,3 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://dapi.kakao.com", // 카카오 맵 API 서버 URL
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
-  // },
-});
+export default defineConfig({ plugins: [react()] });
